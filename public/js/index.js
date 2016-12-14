708e7613f7aaf760ea0931a6fb26a4b81432a1a0
no502zhang <no502zhang@163.com>
2016-12-14 18:01:22 +0800

    菜单使用组件显示

--- public/js/index.js
+++ public/js/index.js
@@ -1,41 +1,33 @@
 var user = 'no502zhang';
+var articlePath = 'blog';
 
-var blog;
+Vue.component('blog-sidebar', {
+    props: ['name', 'items'],
+    template: '#blog-sidebar-template'
+})
+
+var blog = new Vue({
+    el: '#blog',
+    data: {
+        dirs: [{ "url": "/articles", "name": "文章" }],
+        files: [{ "url": "", "name": "" }],
+        article: '# Hello World!'
+    },
+    created: function() {
+        getBlogList(articlePath);
+    },
+    computed: {
+        compiledMarkdown: function() {
+            return marked(this.article, { sanitize: true })
+        }
+    }
+});
+
 /**
  * 初始化
  */
-$(document).ready(function () {
+$(document).ready(function() {
     $(document).attr('title', user);
-
-    sidebar = new Vue({
-        el: '#sidebar',
-        data: {
-            blogs: {}
-        },
-        created: function () {
-            getBlogList('blog');
-        },
-        methods: {
-            dirClick: function (path) {
-                getBlogList(path);
-            },
-            fileClick: function (blogURL) {
-                getBlogContent(blogURL);
-            }
-        }
-    })
-
-    main = new Vue({
-        el: '#main',
-        data: {
-            message: '# Hello ZHANG!'
-        },
-        computed: {
-            compiledMarkdown: function () {
-                return marked(this.message, { sanitize: true })
-            }
-        }
-    })
 });
 
 /**
@@ -44,8 +36,16 @@ $(document).ready(function () {
 function getBlogList(path) {
     blogListURL = 'https://api.github.com/repos/' + user + '/' + user + '.github.io/contents/' + path;
 
-    $.getJSON(blogListURL, function (json) {
-        sidebar.blogs = json;
+    $.getJSON(blogListURL, function(json) {
+        blog.dirs.splice(0, blog.dirs.length);
+        blog.files.splice(0, blog.files.length);
+        for (var i = 0; i < json.length; i++) {
+            if (json.type == 'dir') {
+                blog.dirs.push({ "url": json[i].path, "name": json[i].name });
+            } else if (json.type == 'file') {
+                blog.files.push({ "url": json[i].path, "name": json[i].name });
+            }
+        }
     });
 }
 
@@ -53,7 +53,7 @@ function getBlogList(path) {
  * 获取博客内容
  */
 function getBlogContent(blogURL) {
-    $.get(blogURL, function (result) {
-        main.message = result;
+    $.get(blogURL, function(result) {
+        blog.article = result;
     });
 }
